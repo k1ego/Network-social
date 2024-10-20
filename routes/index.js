54796aaebbf37ddb142b232499399b8e130d7575
k1ego <ladonin05@mail.ru>
2024-10-21 01:17:57 +0300

    like-controller добавлен

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const { UserController, PostController, CommentController } = require('../controllers');
-const { authenticateToken } = require("../middleware/auth");
-
+const {
+	UserController,
+	PostController,
+	CommentController,
+	LikeController,
+} = require('../controllers');
+const { authenticateToken } = require('../middleware/auth');
 
 const uploadDestination = 'uploads';
 
 // Показываем, где хранить файлы
 const storage = multer.diskStorage({
 	destination: uploadDestination,
-	filename: function(req, file, cb) {
-		cb(null, file.originalname)
-	}
+	filename: function (req, file, cb) {
+		cb(null, file.originalname);
+	},
 });
 
 const uploads = multer({ storage: storage });
@@ -21,8 +25,8 @@ const uploads = multer({ storage: storage });
 router.post('/register', UserController.register);
 router.post('/login', UserController.login);
 router.get('/current', authenticateToken, UserController.current);
-router.get('/users/:id', authenticateToken,  UserController.getUserById);
-router.put('/users/:id', authenticateToken,  UserController.updateUser);
+router.get('/users/:id', authenticateToken, UserController.getUserById);
+router.put('/users/:id', authenticateToken, UserController.updateUser);
 
 // Роуты постов
 router.post('/posts', authenticateToken, PostController.createPost);
@@ -32,7 +36,14 @@ router.delete('/posts/:id', authenticateToken, PostController.deletePost);
 
 // Роуты комментариев
 router.post('/comments', authenticateToken, CommentController.createComment);
-router.delete('/comments/:id', authenticateToken, CommentController.deleteComment);
+router.delete(
+	'/comments/:id',
+	authenticateToken,
+	CommentController.deleteComment
+);
 
+// Роуты лайков
+router.post('/likes', authenticateToken, LikeController.likePost);
+router.delete('/likes/:id', authenticateToken, LikeController.unlikePost);
 
 module.exports = router;
