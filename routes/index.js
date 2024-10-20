9d337b4a36a87d5158290046bb07b5a3170f85ec
k1ego <ladonin05@mail.ru>
2024-10-21 01:56:08 +0300

    follow-controller добавлен

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const {
 	PostController,
 	CommentController,
 	LikeController,
+	FollowController,
 } = require('../controllers');
 const { authenticateToken } = require('../middleware/auth');
 
@@ -45,5 +46,9 @@ router.delete(
 // Роуты лайков
 router.post('/likes', authenticateToken, LikeController.likePost);
 router.delete('/likes/:id', authenticateToken, LikeController.unlikePost);
+
+// Роуты подписок
+router.post('/follow', authenticateToken, FollowController.followUser);
+router.delete('/unfollow/:id', authenticateToken, FollowController.unfollowUser);
 
 module.exports = router;
